a0799837d740d4e24de9d149b558261af56f3ce0
cmsbuild <>
2011-10-10 14:10:43 +0000

    This commit was manufactured by cvs2git to create tag 'RecoTracker- TkTrackingRegions-V01-03-07'.
    Sprout from master 2011-07-06 11:47:26 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'make the etaphi lookup optional'
    Cherrypick from master 2011-10-10 14:08:28 UTC Matti Kortelainen <sha1-6867f286182089adb2ae9be6506b93ff6bac3bbd@cern.ch> 'Performance improvement':
        src/RectangularEtaPhiTrackingRegion.cc

--- src/RectangularEtaPhiTrackingRegion.cc
+++ src/RectangularEtaPhiTrackingRegion.cc
@@ -312,8 +312,8 @@ TrackingRegion::Hits RectangularEtaPhiTrackingRegion::hits(
     est = estimator(&fl,es);
   }
   
-  static EtaPhiMeasurementEstimator etaPhiEstimator ((theEtaRange.second-theEtaRange.first)/2.,
-						     (thePhiMargin.left()+thePhiMargin.right())/2.);
+  EtaPhiMeasurementEstimator etaPhiEstimator ((theEtaRange.second-theEtaRange.first)/2.,
+					      (thePhiMargin.left()+thePhiMargin.right())/2.);
   MeasurementEstimator & findDetAndHits = etaPhiEstimator;
   if (est){
     LogDebug("RectangularEtaPhiTrackingRegion")<<"use pixel specific estimator.";
@@ -369,8 +369,7 @@ TrackingRegion::Hits RectangularEtaPhiTrackingRegion::hits(
 
   TrackingRegion::Hits layerHits = layer->hits(ev,es);
   for (TrackingRegion::Hits::const_iterator ih= layerHits.begin(); ih != layerHits.end(); ih++) {
-    const TrackingRecHit * hit = (*ih)->hit();
-    if ( est->hitCompatibility()(hit,es) ) {
+    if ( est->hitCompatibility()(ih->get()) ) {
        result.push_back( *ih );
     }
   }
